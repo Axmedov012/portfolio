42b3e91ebdbb934c71607d76b0e18532c9ce6a8e
doniyor <doniyoraxmedov012@gmail.com>
2023-10-09 23:01:38 +0500

    work

---
+++ yiiapp/api/backend/config/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+yii::setAlias('@common',dirname(__DIR__));
+yii::setAlias('@frontend',dirname(dirname(__DIR__)) .'/frontend');
+yii::setAlias('@backend',dirname(dirname(__DIR__)) .'/backend');
+yii::setAlias('@console',dirname(dirname(__DIR__)) .'/console');
+yii::setAlias('@api',dirname(dirname(__DIR__)) .'/api');
