97ab73a816e3d335997efb0e47e0cebc0ca640c0
doniyor <doniyoraxmedov522@gmail.com>
2023-10-09 22:49:53 +0500

    frame

---
+++ yii2/admin_page.php
@@ -0,0 +1,28 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>admin page</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <h3>Hi,<span>Admin</span></h3>
+            <h1>Welcome </h1>
+            <p>this is an admin page</p>
+            <a href="login_form.php" class="btn"> login</a>
+            <a href="register_form.php" class="btn"> register</a>
+            <a href="logout.php" class="btn">logout</a>
+        </div>
+    </div>
+
+</body>
+
+</html>
