9dd98691b8b191414bd4020e4d71116e8f628fef
doniyor <doniyoraxmedov012@gmail.com>
2023-10-18 16:00:39 +0500

    framework

---
+++ yiiapp/frontend/controllers/DsnController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class DsnController extends Controller
+{
+    public function actionIndex()
+    {
+        $sql = "update person set gender = 'ayol' where name='momin' ";
+
+        $command = \Yii::$app->db->createCommand()
+            ->batchInsert('person',['name','email','gender'],
+                [
+                    ['nodir','nodir012@gmail.com','erkak'],
+                    ['temur','temur012@gmail.com','erkak'],
+                    ['nilufar','nilufar012@gmail.com','ayol'],
+               ])
+            ->execute();
+
+        return $this->render("index");
+    }
+}
